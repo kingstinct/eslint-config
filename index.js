2b139076f664a75e14f920cd19818da701390c3a
Robert Herber <robert@herber.me>
2017-06-20 09:07:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:jest/recommended',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react'
+  ],
+  installedESLint: true,
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+    'react-native',
+    'flowtype',
+    'lodash-fp',
+    'promise',
+    'jest',
+    'json',
+    'prettier'
+  ],
+  ecmaFeatures: {
+    jsx: true
+  },
+  rules: {
+    'class-methods-use-this': 0,
+    'global-require': 0,
+    'lodash-fp/use-fp': 'error',
+    'no-confusing-arrow': 0,
+    'no-nested-ternary': 0,
+    'no-return-assign': 0,
+    'no-unused-expressions': 0,
+    'one-var': 0,
+    'prettier/prettier': 0,
+    'promise/catch-or-return': 2,
+    'promise/no-native': 2,
+    'react-native/no-color-literals': 0,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-bind': 0,
+    'react/prefer-stateless-function': 0,
+    'react/sort-comp': 0
+  },
+  env: {
+    'jest/globals': true
+  },
+  globals: {
+    fetch: true,
+    __DEV__: true,
+    __OFFLINE__: true,
+    window: true
+  },
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true
+    }
+  }
+};
